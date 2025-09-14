882bfaf36ef6b2a41b61017fd097df08cb76f4fa
Ekiller98 <ethan.nunley98@gmail.com>
2025-09-14 13:55:46 -0400

    -setup DB context with options, -added Cors -created Employee Db set, interface, and repository -added the employee repository to the dependency injection

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using EmployeeManagement.Repositories;
+{
+    
+}
+
 namespace EmployeeManagement
 {
     public class Program
@@ -5,7 +12,28 @@ namespace EmployeeManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<Data.AppDbContext>(options =>
+                options.UseInMemoryDatabase("EmployeeDb"));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors",
+                    builder =>
+                    {
+                        builder.WithOrigins("http//localhost:4200")
+                               .AllowAnyMethod()
+                               .AllowAnyHeader();
+                    });
+            });
+            //add employee repository to the dependecy injection(DI)
+            builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            builder.Services.AddControllers();
+
             var app = builder.Build();
+
+            app.UseCors("MyCors");
 
             app.MapGet("/", () => "Hello World!");
 
---
+++ EmployeeManagement/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        //Get all employees
+        Task<IEnumerable<Employee>> GetAllAsync();
+
+        //Get employee by id
+        Task<Employee?> GetByIdAsync(int id);
+
+        //Add new employee(post)
+        Task AddEmployeeAsync(Employee employee);
+
+        //update employee(put)
+        Task UpdateEmployeeAsync(Employee employee);
+
+        //delete employee
+        Task DeleteEmployeeAsync(int id);
+
+    }
+}
